7978c39e509a7e8b24687abcaeffbe71a0a9e28e
Rahulsaraf1578 <rahulsaraf1578@gmail.com>
2021-05-24 14:56:01 +0530

    see service page

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 //creating a database
 mongoose.connect("mongodb://localhost:27017/workwithus", {
     useCreateIndex:false,
-    useNewUrlParser:false,
-    useUnifiedTopology:false
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+
 }).then(() => {
     console.log("connection succesful");
 }).catch((error) => {
